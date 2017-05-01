02fe37afd09faee695cb84b9f10063b275ca0dd6
JV-BugMaker <794241063@qq.com>
2017-05-01 19:59:49 +0800

    [*]修改

--- src/go/interface.go
+++ src/go/interface.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/rogpeppe/godef/go/ast"
+
 //接口也是一种结构
 
 //不能有字段
@@ -51,6 +53,31 @@ type tester interface {
 }
 
 
+//执行机制
+//接口使用一个名为itab的结构存储运行期间所需的相关类型信息
 
+type iface struct {
+	tab *itab  //类型信息
+	data unsafe.Pointer //实际对象指针
+}
+
+type itab struct {
+	inter *interfaceType //接口类型
+	_type *_type //实际对象类型
+	fun [1]uintptr //实际对象方法地址
+}
+
+//将对象赋值给接口对象变量时，会复制该对象
+
+//我们无法修改接口存储的复制品 因为它是unaddressable的
+
+
+func main2(){
+	//将指针赋值给接口变量
+	var t interface{} = &d
+
+	//操作指针 进行修改
+	t.(*data).x = 200
+}
 
 
