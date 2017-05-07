f81051da1131f9f98890f44ea2c6135aed0d54dc
JV-BugMaker <794241063@qq.com>
2017-05-07 19:38:21 +0800

    [*]wait方式

--- src/go/parallesim.go
+++ src/go/parallesim.go
@@ -14,24 +14,42 @@ import "time"
 //关键字go并非执行并发操作 而是创建一个并发任务单元 会被放置在系统队列中
 
 
-var c int
+//var c int
+//
+//func counter() int{
+//	c++
+//	return c
+//}
+////goroutine会跟defer一样  延迟执行  会发生参数立即复制
+//func main() {
+//	a := 100
+//
+//	go func(x ,y int) {
+//		time.Sleep(time.Second) //让goroutine在main逻辑之后执行
+//		println("go:",x,y)
+//	}(a,counter())   //立即计算并复制参数
+//
+//	a += 10
+//
+//	println("main:",a,counter())
+//
+//	time.Sleep(time.Second * 3)
+//}
 
-func counter() int{
-	c++
-	return c
-}
-//goroutine会跟defer一样  延迟执行  会发生参数立即复制
 func main() {
-	a := 100
+	exit := make(chan struct{})  //创建通道 因为仅是通知 数据并没有实际意义
 
-	go func(x ,y int) {
-		time.Sleep(time.Second) //让goroutine在main逻辑之后执行
-		println("go:",x,y)
-	}(a,counter())   //立即计算并复制参数
+	go func() {
+		time.Sleep(time.Second)
+		println("goroutine done.")
 
-	a += 10
+		close(exit)   //关闭通道发出信号
+	}()
 
-	println("main:",a,counter())
 
-	time.Sleep(time.Second * 3)
+	println("main ...")
+
+	<-exit   //如收到通道关闭的通知 就解除阻塞 否则处于等待状态
+
+	println("main exit.")
 }
