5c9de0cf3d0da6f62d0669e3fe3ac8c5615b7e0d
JV-BugMaker <794241063@qq.com>
2017-05-20 11:50:26 +0800

    [*]单向通道

--- src/go/oneway.go
+++ src/go/oneway.go
@@ -1,3 +1,37 @@
 package main
 
+import "sync"
+//通道默认是双向的 并不区分发送和接收端 我们可以限制收发操作的方向来获取更严谨的逻辑
 
+//通常使用类型转换来获取单向通道
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	c := make(chan int)
+	var send chan<- int = c
+	var recv <-chan int = c
+	//接收方
+	go func() {
+		defer wg.Done()
+
+		for x := range recv {
+			println(x)
+		}
+	}()
+	//发送方
+	go func() {
+		defer wg.Done()
+		defer close(c)
+
+		for i := 0;i<3;i++{
+			send <- i
+		}
+	}()
+
+	wg.Wait()
+	//不能再单向通道上做逆向操作
+	//同样 close不能用于接收端 只能作用于发送端
+}
