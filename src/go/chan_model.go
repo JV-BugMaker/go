3e7da5a38f257d928cf8f2149d2161d916384eba
JV-BugMaker <794241063@qq.com>
2017-05-20 12:35:29 +0800

    [*]ID pool

--- src/go/chan_model.go
+++ src/go/chan_model.go
@@ -35,4 +35,32 @@ func main() {
 
 	close(r.data)		//关闭通道 发出结束通知
 	r.Wait()		//等待接收者处理结束
+
+	//鉴于通道本身 就是一个并发安全的队列 可用作ID generator Pool等用途
+
+}
+
+type pool chan []byte
+
+func newPool(cap int) pool {
+	return make(chan []byte,cap)
+}
+
+func (p pool) get() []byte{
+	var v []byte
+
+	select {
+	case v<-p:  	//返回
+	default:	//返回失败 新建
+		v = make([]byte,10)
+	}
+
+	return v
+}
+
+func (p pool) put(b []byte) {
+	select {
+	case p<-b:		//放回
+	default:		//放回失败
+	}
 }
